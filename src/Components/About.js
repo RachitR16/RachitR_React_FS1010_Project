f2e1941f5041db08a16dadec293330549d8e90dc
rachit raval <rachit.raval@gmail.com>
2020-06-27 14:40:54 -0400

    revision

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,13 +3,13 @@ import './About.css'
 
 function About(){
     return (
-        <div className="about" id="about">
+        <section className="about" id="about">
             <div className="content">
-                <img className="profile" src="/images/rachit.png" alt=""/>
+                <img className="profile" src="/images/rachit.png" alt="profile_img"/>
                 <h2>about me</h2>
                 <p className="aboutMe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita rem ab beatae explicabo, porro similique amet quam earum cupiditate totam molestiae quaerat maxime nam aut nisi reprehenderit voluptatem dolor dolorum.</p>
             </div>
-        </div>
+        </section>
     )
 }
 
